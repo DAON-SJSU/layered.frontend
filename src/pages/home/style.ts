d199454bc7570a46aa0b69ca1ee534d3bf73cdbc
winshine0326 <shane10340@gmail.com>
2025-05-18 15:24:47 +0900

    feat :: Home Container style

--- src/pages/home/style.ts
+++ src/pages/home/style.ts
@@ -2,4 +2,11 @@ import styled from '@emotion/styled';
 
 export const Container = styled.div`
     margin: 24px;
+    display: flex;
+    padding: var(--Gap-24, 24px);
+    flex-direction: column;
+    align-items: flex-start;
+    gap: var(--Gap-20, 20px);
+    flex: 1 0 0;
+    align-self: stretch;
 `
