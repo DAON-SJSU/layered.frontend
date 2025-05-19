14fec952b540594b44ca8a51601f9ade1b4b56b1
bbibbaroni <i89155345@gmail.com>
2025-05-20 02:48:34 +0900

    feat :: App.tsx에서 전체 패이지를 랜더 할때 애니매이션을 구현 했습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import Layout from "./Layout/Layout";
 import Home from "./pages/home/index";
 import Palette from "./pages/palette/index";
 import Playlist from "./pages/playlist/index";
 import Result from "./pages/result/index";
 import Loading from "./pages/loading/index";
-import ChoicePreference from './pages/choicePreference';
-import ChoiceTempo from './pages/choiceTempo';
+import ChoicePreference from "./pages/choicePreference";
+import ChoiceTempo from "./pages/choiceTempo";
+
+const pageVariants = {
+  initial: {
+    opacity: 0,
+    x: 20,
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+  },
+  exit: {
+    opacity: 0,
+    x: -20,
+  },
+};
+
+const pageTransition = {
+  type: "tween",
+  ease: "anticipate",
+  duration: 0.5,
+};
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
+  const location = useLocation();
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -25,17 +48,29 @@ function App() {
   }
 
   return (
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path='/palette' element={<Palette />} />
-          <Route path='choicePreference' element={<ChoicePreference />} />
-          <Route path='choiceTempo' element={<ChoiceTempo />} />
-        </Route>
-        <Route path='/playlist' element={<Playlist />} />
-        <Route path='/result' element={<Result />} />
-      </Routes>
-  )
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={location.pathname}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        variants={pageVariants}
+        transition={pageTransition}
+      >
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/loading" element={<Loading />} />
+            <Route path="/palette" element={<Palette />} />
+            <Route path="choicePreference" element={<ChoicePreference />} />
+            <Route path="choiceTempo" element={<ChoiceTempo />} />
+          </Route>
+          <Route path="/playlist" element={<Playlist />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </motion.div>
+    </AnimatePresence>
+  );
 }
 
 export default App;
