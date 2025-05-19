60defb79434d840e7da1bbbe6a83517fd8c46f44
bbibbaroni <i89155345@gmail.com>
2025-05-19 22:39:03 +0900

    feat :: loading page 퍼블리싱과 홈 접속시 로딩 기능

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,37 @@
-import { Route, Routes } from 'react-router-dom';
-import Layout from './Layout/Layout';
-import Home from './pages/home/index';
-import Palette from './pages/palette/index';
-import Playlist from './pages/playlist/index';
-import Result from './pages/result/index';
+import { Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Layout from "./Layout/Layout";
+import Home from "./pages/home/index";
+import Palette from "./pages/palette/index";
+import Playlist from "./pages/playlist/index";
+import Result from "./pages/result/index";
+import Loading from "./pages/loading/index";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path='/palette' element={<Palette />}/>
-        </Route>
-        <Route path='/playlist' element={<Playlist />}/>
-        <Route path='/result' element={<Result />}/>
-      </Routes>
-  )
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/palette" element={<Palette />} />
+      </Route>
+      <Route path="/playlist" element={<Playlist />} />
+      <Route path="/result" element={<Result />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/loading/index.tsx
@@ -0,0 +1,12 @@
+import * as _ from "./style";
+import LoadingLogo from "../../components/loadingBox/index";
+
+const Loading = () => {
+  return (
+    <_.Container>
+      <LoadingLogo />
+    </_.Container>
+  );
+};
+
+export default Loading;
