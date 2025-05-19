a4bea37857e1b4e488b2774f2c533451cecc7b80
winshine0326 <shane10340@gmail.com>
2025-05-19 20:31:55 +0900

    feat :: layout min height 추가

--- src/Layout/style.ts
+++ src/Layout/style.ts
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 export const Mobile = styled.main`
   height: max(calc(var(--vh, 1vh) * 100),100%);
   width: 100%;
+  min-height: 100vh;
   max-width: 430px;
   margin: 0 auto;
   background: var(--Background);
