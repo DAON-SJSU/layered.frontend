bf9876e457df1cd454284f758175c35903295d3c
winshine0326 <shane10340@gmail.com>
2025-05-28 03:06:53 -0700

    feat :: axios 설정, location 설정

--- src/pages/result/index.tsx
+++ src/pages/result/index.tsx
@@ -1,10 +1,24 @@
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import * as _ from "./style";
 import { emotionMap } from "./emotionmap";
+import axios from 'axios';
+
 
 const Result = () => {
   const { emotion } = useParams();
   const config = emotionMap[emotion ?? ""];
+  const location = useLocation();
+  const { request } = location.state || {};
+
+
+  const handleSendRequest = async () => {
+    try {
+      const response = await axios.post("172.20.3.59:8000", request);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
 
   if (!config) {
     return <div>잘못된 감정입니다.</div>;
@@ -20,7 +34,7 @@ const Result = () => {
           <_.SubText>Your heart says...</_.SubText>
           <_.EmotionText>{label}</_.EmotionText>
           <_.EmotionIcon src={image} alt={label} />
-          <_.PlaylistButton buttonColor={buttonColor}>Check Your Playlist</_.PlaylistButton>
+          <_.PlaylistButton buttonColor={buttonColor} onClick={() => { handleSendRequest }}>Check Your Playlist</_.PlaylistButton>
           <_.RetryText>No Thanks, I wanna try again.</_.RetryText>
         </_.Container>
       </_.MainBody>
